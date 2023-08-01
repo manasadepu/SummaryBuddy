c6a16f9a846facb1a9456640ac8af34d298a1094
Manas A <112828012+manasadepu@users.noreply.github.com>
2023-08-01 01:17:56 -0400

    added stuff for heroku deployment

--- app.py
+++ app.py
@@ -5,30 +5,39 @@ from sumy.summarizers.text_rank import TextRankSummarizer
 
 app = Flask(__name__)
 
-@app.route("/<text>", methods=['GET'])
+@app.route("/getsummary/<text>", methods=['GET'])
 def get_summary(text):
 
-    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    if not text:
+        error_msg = {
+            "ERROR": "No text found"
+       }
+        
+        return jsonify(error_msg)
+    
+    else:
+        parser = PlaintextParser.from_string(text, Tokenizer("english"))
+        summarizer = TextRankSummarizer()
+        SUMMARYLENGTH = 5
 
-    summarizer = TextRankSummarizer()
+        summary = summarizer(parser.document, SUMMARYLENGTH)
+
+        large_sentence = ""
+        for sentence in summary:
+            large_sentence += " " + str(sentence)
+
+        text_response = {
+            "text": large_sentence,
+            "success": True,
+            "accessed": "Yes, access successful"
+        }
+
+        return jsonify(text_response), 200
+
+@app.route("/")
+def index():
+    return "<h1>Welcome to summary buddy!</h1>"
 
 
-    SUMMARYLENGTH = 5
-
-    summary = summarizer(parser.document, SUMMARYLENGTH)
-
-    large_sentence = ""
-
-    for sentence in summary:
-        large_sentence += " " + str(sentence)
-
-    text_response = {
-        "text": large_sentence,
-        "success": True,
-        "accessed": "Yes, access successful"
-    }
-
-
-    return jsonify(text_response), 200
-
-
+if __name__ == '__main__':
+    app.run(threaded=True, debug=True, port=5000)
